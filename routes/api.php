b137b1dbf9bc352a94272f3a28e865c1cd383b3b
Ryan <127997456+JuniorDeveloper17@users.noreply.github.com>
2025-02-17 15:24:30 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::get('/terbaru', [MainController::class, 'terbaru']);
 Route::get('/popular', [MainController::class, 'popular']);
+Route::get('/berwarna/{page}', [MainController::class, 'berwarna']);
+
+Route::get('/genre', [MainController::class, 'genre']);
+Route::get('/genre/{genre}/{page}', [MainController::class, 'listgenre']);
+
+Route::get('/theme', [MainController::class, 'theme']);
+Route::get('/theme/{theme}/{page}', [MainController::class, 'listtheme']);
+
+Route::get('/jenis', [MainController::class, 'jenis']);
+Route::get('/jenis/{jenis}/{page}', [MainController::class, 'listjenis']);
+
+Route::get('/detail/{id}', [MainController::class, 'detail']);
+
+Route::get('/baca/{id}', [MainController::class, 'baca']);
